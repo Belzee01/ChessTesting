46d114df76828bdeee13125c59d84e0bb6bfa908
krzysztof_stachanczyk <krzysztof.stachanczyk1995@gmail.com>
2016-05-23 13:23:59 +0200

    COUNTER IMPLEMENTATION 2

--- src/sample/services/CounterService.java
+++ src/sample/services/CounterService.java
@@ -8,33 +8,67 @@ import java.time.Duration;
 import java.util.TimerTask;
 import java.util.Timer;
 import java.util.function.Consumer;
+import java.lang.IllegalStateException;
 
 /**
  * Created by krzysztof on 23.05.16.
  */
 public class CounterService extends TimerTask{
     private Duration duration =Duration.ofSeconds(0);
+    private Duration targetDuration = Duration.ofSeconds(0);
+    private boolean timeOutEnable=false;
+    private boolean timerRun=false;
     private Timer timer1 = new Timer();
 
     @Setter
-    public Consumer<Duration> onTimeUp;
+    public Consumer<Duration> onTick;
+
+    @Setter
+    public Consumer<Duration> onTimeOut;
 
     public void startTiming(){
-        timer1.schedule (this, 0, 1000);
+        if(timerRun) {
+           stopTiming();
+        }
+
+        timer1.schedule(this, 0, 1000);
+        timerRun=true;
     }
     public void stopTiming(){
-        timer1.cancel();
+        if(timerRun) {
+            timer1.cancel();
+            timerRun=false;
+        }
     }
 
+    public void enableTimeOutMode(Duration timeOut){
+        reset();
+        targetDuration=timeOut;
+        timeOutEnable=true;
+    }
+
+    public void disableTimeOutMode(){
+        reset();
+        timeOutEnable=false;
+    }
 
     public void reset(){
+        stopTiming();
         duration=Duration.ofSeconds(0);
+        startTiming();
     }
 
     public void run(){
         duration=duration.plusSeconds(1);
-        if(onTimeUp!=null){
-            onTimeUp.accept(duration);
+        if(onTick!=null){
+            onTick.accept(duration);
+        }
+
+        if(timeOutEnable && !duration.minus(targetDuration).isNegative() ){
+            if(onTimeOut!=null) {
+                onTimeOut.accept(duration);
+            }
+            stopTiming();
         }
     }
 }
